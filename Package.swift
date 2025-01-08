43e508f704ed1eff90fc8810bd112f1d27b605e6
andrellaflame <sulimenkoandrey04@gmail.com>
2025-01-08 09:30:51 -0500

    Package.swift modified
    Signed-off-by: andrellaflame <sulimenkoandrey04@gmail.com>

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "StabilityAssuranceTool",
     platforms: [
-        .iOS(.v13),
         .macOS(.v10_15),
     ],
     products: [
@@ -17,8 +16,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "StabilityAssuranceTool",
             dependencies: [
