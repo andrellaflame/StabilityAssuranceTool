d13c2b09faf56e3838929ecb17a65db0ef1e4896
andrellaflame <sulimenkoandrey04@gmail.com>
2025-04-21 13:11:00 -0400

    Command Plugin wrapper added
    Signed-off-by: andrellaflame <sulimenkoandrey04@gmail.com>

---
+++ Plugins/SAT Command Plugin/CommandContext.swift
@@ -0,0 +1,99 @@
+//
+//  CommandContext.swift
+//  StabilityAssuranceTool
+//
+//  Created by Andrii Sulimenko on 2025-04-21.
+//
+
+import PackagePlugin
+
+protocol CommandContext {
+    var tool: String { get throws }
+
+    var cacheDirectory: String { get }
+
+    var workingDirectory: String { get }
+
+    var unitName: String { get }
+
+    var subUnitName: String { get }
+
+    func targets(named names: [String]) throws -> [(paths: [String], name: String)]
+}
+
+extension PluginContext: CommandContext {
+    var tool: String {
+        get throws {
+            try tool(named: "sat").path.string
+        }
+    }
+
+    var cacheDirectory: String {
+        pluginWorkDirectory.string
+    }
+
+    var workingDirectory: String {
+        package.directory.string
+    }
+
+    var unitName: String {
+        "package"
+    }
+
+    var subUnitName: String {
+        "module"
+    }
+
+    func targets(named names: [String]) throws -> [(paths: [String], name: String)] {
+        let targets = names.isEmpty
+            ? package.targets
+            : try package.targets(named: names)
+        return targets.compactMap { target in
+            guard let target = target.sourceModule else {
+                Diagnostics.warning("Target '\(target.name)' is not a source module; skipping it")
+                return nil
+            }
+
+            return (paths: [target.directory.string], name: target.name)
+        }
+    }
+}
+
+#if canImport(XcodeProjectPlugin)
+
+import XcodeProjectPlugin
+
+extension XcodePluginContext: CommandContext {
+    var tool: String {
+        get throws {
+            try tool(named: "sat").path.string
+        }
+    }
+
+    var cacheDirectory: String {
+        pluginWorkDirectory.string
+    }
+
+    var workingDirectory: String {
+        xcodeProject.directory.string
+    }
+
+    var unitName: String {
+        "project"
+    }
+
+    var subUnitName: String {
+        "target"
+    }
+
+    func targets(named names: [String]) throws -> [(paths: [String], name: String)] {
+        if names.isEmpty {
+            return [(paths: [xcodeProject.directory.string], name: xcodeProject.displayName)]
+        }
+        return xcodeProject.targets
+            .filter { names.contains($0.displayName) }
+            .map { (paths: $0.inputFiles.map(\.path.string).filter { $0.hasSuffix(".swift") }, name: $0.displayName) }
+    }
+}
+
+#endif
